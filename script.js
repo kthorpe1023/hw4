1b969e3ee0f384a6744d20d052db8cb38fd9d6b2
Katie Thorpe <kthorpe1023@gmail.com>
2020-04-19 15:50:35 -0600

    subtract time on wrong and save scores to local storage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // VARIABLES
 var score = 0;
 var timer = 60;
+var startTime;
 var displayTimer = document.querySelector("#timer");
 var presentScore = document.getElementById("score");
 var finalScore = document.getElementById("display-score");
@@ -10,6 +11,9 @@ var question = document.querySelector("#questionOne");
 var question3 = document.querySelector("#question-three");
 var question4 = document.querySelector("#question-four");
 var final = document.querySelector("#final-screen");
+var submitBtn = document.querySelector("#submit");
+var initials = document.querySelector("#enter-initials");
+
 
 
 //  FUNCTIONS
@@ -22,19 +26,27 @@ function changeScreen() {
         
       }
       question.classList.remove("hide");
-   
+      startQuizTimer();
       questionOne();
 }
-
-  var startTimer = setInterval(function(){
+function startQuizTimer(){
+  startTimer = setInterval(function(){
     timer--;
     displayTimer.textContent = timer;
     if(timer == 0){
       clearInterval(startTimer); 
-      alert("You ran out of time!")
+      alert("You ran out of time!");
+      start.classList.add("hide");
+      question.classList.add("hide");
+      question2.classList.add("hide");
+      question3.classList.add("hide");
+      question4.classList.add("hide");
+      finalScreenwrong();
     }
     
   }, 1000);
+}
+ 
   
   
 
@@ -66,7 +78,7 @@ function questionTwo() {
 }
 
 function questionTwowrong() {
-  alert("that wassn't right!");
+  timer -= 5;
   question.classList.add("hide");
   question2.classList.remove("hide");
   var ansBtn1 = document.querySelector("#ans2Btn1");
@@ -93,7 +105,7 @@ function questionThree(){
   presentScore.textContent = score;
 }
 function questionThreewrong(){
-  alert("that wassn't right!")
+  timer -= 5;
   question2.classList.add("hide");
   question3.classList.remove("hide");
   var ans3Btn1 = document.querySelector("#ans3Btn1");
@@ -103,7 +115,7 @@ function questionThreewrong(){
   ans3Btn1.addEventListener("click", questionFourwrong);
   ans3Btn2.addEventListener("click", questionFour);
   ans3Btn3.addEventListener("click", questionFourwrong);
-  ans3Btn4.addEventListener("click", questionFourWrong);
+  ans3Btn4.addEventListener("click", questionFourwrong);
 }
 
 function questionFour(){
@@ -119,6 +131,7 @@ function questionFour(){
   presentScore.textContent = score;
 }
 function questionFourwrong(){
+  timer -= 5;
   question3.classList.add("hide");
   question4.classList.remove("hide");
   var ans4Btn1 = document.querySelector("#ans4Btn1");
@@ -140,16 +153,54 @@ function finalScreen(){
   if(timer !== 0){
     clearInterval(startTimer);
   }
+  
 
 }
 function finalScreenwrong(){
+  timer -= 5;
   question4.classList.add("hide");
   final.classList.remove("hide");
+  presentScore.textContent = score;
   finalScore.textContent = score;
   if(timer !== 0){
     clearInterval(startTimer);
   }
 }
+// create object to store user score, initials, and remaining time
+
+
+//add function to add score and initials to local storage
+function submitScore(){
+  var savedScores = localStorage.getItem("myScore");
+  var scores = [];
+  var pushOldScores = JSON.parse(savedScores);
+  scores.push(pushOldScores);
+  
+  // console.log(savedScores);
+  // console.log(typeof scores);
+
+  
+    
+  var userInitials = initials.value;
+  var myScore = {
+      score: score,
+      initials: userInitials,
+      timeLeft: timer
+  }
+   
+    scores.push(myScore);
+  // localStorage.setItem("timer", "score", "userInitials");
+
+  var myJSON = JSON.stringify(scores);
+  localStorage.setItem("myScore", myJSON);
+
+}
+
+//add event listener to submit initials
+submitBtn.addEventListener("click", function(e){
+  e.preventDefault();
+  submitScore();
+})
 
 
 // Add event listener to begin
